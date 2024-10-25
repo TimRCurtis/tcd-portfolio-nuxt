cec2e3224f34acc481a4efd0d758345238b2e75e
Tim Curtis <timrcurtis@gmail.com>
2024-10-25 18:59:13 -0400

    finished navbar component and added logo

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <nav class="bg-white font-rubik w-full border-b border-gray-200">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="~/public/tcd-logo.svg" class="h-8" alt="Tim Curtis Designs Logo">
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tim Curtis Designs</span>
+                </a>
+             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <button type="button" class="text-white bg-tcd-mariner-600 hover:bg-tcd-mariner-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">My CV</button>
+                </div>
+            </div>
+        </nav>
+     </header>
+</template>
