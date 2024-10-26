c9eb81864c679236933b47c0532191b49a3480fc
Tim Curtis <timrcurtis@gmail.com>
2024-10-25 21:29:34 -0400

    updated intro section

--- app.vue
+++ app.vue
@@ -1,16 +1,6 @@
 <template>
   <Navbar />
-  <section class="w-full max-w-7xl mx-auto px-4 md:px-6 py-32">
-    <!-- todo Introduction Section -->
-     <!-- todo Contact Button - mailto:timrcurtisdesigns@gmail.com -->
-    <div class="flex flex-col justify-between items-center">
-      <h1 class="font-rubik font-bold mb-24 text-6xl text-tcd-java-400">Hi, I'm Tim, <br>web developer.</h1>
-      <div class="flex flex-row justify-between items-start">
-        <p class="w-3/4 text-lg font-medium">Experienced Web Developer skilled in creating effective, responsive websites with a focus on problem-solving and team collaboration. I specialize in finding the best solutions for each project, big or small, and committed to continuously learning the latest web technologies to deliver top-quality results.</p>
-        <button href="mailto:timrcurtisdesigns@gmail.com" class="rounded-md font-rubik font-semibold bg-tcd-mariner-800 text-white px-6 py-4 ring ring-tcd-mariner-700 ring-offset-2 hover:bg-tcd-mariner-700">Contact Me</button>
-      </div>
-    </div>
-  </section>
+  <Intro />
   <section class="w-full max-w-7xl mx-auto px-4 md:px-6 py-6">
     <h1 class="font-rubik font-bold pb-8 text-5xl text-tcd-java-400">Projects</h1>
     <div class="flex flex-col">
@@ -30,8 +20,8 @@
 <script setup>
   import ProjectCard from '~/components/ProjectCard.vue';
   import Timeline from '~/components/Timeline.vue';
-  import EducationSection from '~/components/EducationSection.vue';
   import Navbar from '~/components/Navbar.vue';
+  import Intro from '~/components/Intro.vue';
 
   const project1 = {
     title: 'Project 1',
