3b0104dca5fd2a683ac9008e8856275229dc7634
Tim Curtis <timrcurtis@gmail.com>
2024-10-25 21:11:30 -0400

    added intro section with CTA button and edited navbar button styling

--- app.vue
+++ app.vue
@@ -1,8 +1,15 @@
 <template>
   <Navbar />
-  <section>
+  <section class="w-full max-w-7xl mx-auto px-4 md:px-6 py-32">
     <!-- todo Introduction Section -->
      <!-- todo Contact Button - mailto:timrcurtisdesigns@gmail.com -->
+    <div class="flex flex-col justify-between items-center">
+      <h1 class="font-rubik font-bold mb-24 text-6xl text-tcd-java-400">Hi, I'm Tim, <br>web developer.</h1>
+      <div class="flex flex-row justify-between items-start">
+        <p class="w-3/4 text-lg font-medium">Experienced Web Developer skilled in creating effective, responsive websites with a focus on problem-solving and team collaboration. I specialize in finding the best solutions for each project, big or small, and committed to continuously learning the latest web technologies to deliver top-quality results.</p>
+        <button href="mailto:timrcurtisdesigns@gmail.com" class="rounded-md font-rubik font-semibold bg-tcd-mariner-800 text-white px-6 py-4 ring ring-tcd-mariner-700 ring-offset-2 hover:bg-tcd-mariner-700">Contact Me</button>
+      </div>
+    </div>
   </section>
   <section class="w-full max-w-7xl mx-auto px-4 md:px-6 py-6">
     <h1 class="font-rubik font-bold pb-8 text-5xl text-tcd-java-400">Projects</h1>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,7 +6,7 @@
                     <img src="~/public/tcd-logo.svg" class="h-8" alt="Tim Curtis Designs Logo">
                 </a>
              <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button" class="text-white bg-tcd-mariner-800 hover:bg-tcd-mariner-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">My CV</button>
+                    <button type="button" class="text-white bg-tcd-mariner-800 hover:bg-tcd-mariner-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md font-rubik text-sm px-4 py-2 text-center">My CV</button>
                 </div>
             </div>
         </nav>
